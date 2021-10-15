f6cfbf9ce88781245478857fd60267b1548b5853
Ivan Petak <petak@neuralab.net>
2021-10-15 18:00:53 +0200

    Add source files to production version

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+// Using Laravel Mix to build assets, to learn more about it: https://laravel.com/docs/8.x/mix#introduction
 let mix = require('laravel-mix');
 
 mix.js('assets/src/js/rfw-admin.js', 'assets/dist/js/rfw-admin.js');
